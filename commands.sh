a13189db2ad7a2190bc5f32ef901cb66a04a75cc
Chao Xu <xux1912@gmail.com>
2017-08-17 16:17:35 -0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -2,3 +2,9 @@
 
 ##log on
 ssh -i "CpgSite.pem" ec2-user@ec2-34-203-200-196.compute-1.amazonaws.com
+
+
+## clean non-standard chromosome 
+sed -i "/chrUn/d" Conserved_TFBS
+sed -i "/hap[1-9]\t/d" Conserved_TFBS
+sed -i "/random\t/d" Conserved_TFBS
